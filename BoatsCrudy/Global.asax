3d70f46aa34f6eae9a8603ead37f0a493927ba6e
vmandic <mandic.vedran@gmail.com>
2017-05-24 23:23:02 +0200

    initial commit, blank template, updated nugets

---
+++ BoatsCrudy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoatsCrudy.MvcApplication" Language="C#" %>
---
+++ BoatsCrudy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoatsCrudy.Startup))]
+namespace BoatsCrudy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
